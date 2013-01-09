44a93712ec82c7c584fee366bc746891f2a5e177
David Blevins <dblevins@apache.org>
2013-01-09 07:07:29 +0000

    no need to run sql scripts for this test

--- try.sh
+++ try.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./runtests --env -nc -c -U -w tomee com.sun.ts.tests.ejb30.lite.basic.stateful
+./runtests -sql skip --env -nc -c -U -w tomee com.sun.ts.tests.ejb30.lite.basic.stateful
