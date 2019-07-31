4bfd21bf50d0d1ecabf7f70a6cb3d323836f8aeb
Jean-Louis Monteiro <jeanouii@gmail.com>
2019-07-31 22:29:29 +0200

    Updated version of all packages

--- generate-section-packages.sh
+++ generate-section-packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-TCK_HOME="$(perl -ne 'print if s/.*<javaee6.cts.home>(.*)<.*/$1/' $HOME/.m2/settings.xml)"
+TCK_HOME="$(perl -ne 'print if s/.*<javaee8.cts.home>(.*)<.*/$1/' $HOME/.m2/settings.xml)"
 
 find $TCK_HOME/src/com/sun/ts/tests -name '*.java' -exec perl -ne 'print if s/^package +(.*);.*/$1/;' {} \; | egrep -v '\.common($|\.)'| sort | uniq
+find $TCK_HOME/src/com/ibm/jbatch/tck -name '*.java' -exec perl -ne 'print if s/^package +(.*);.*/$1/;' {} \; | egrep -v '\.common($|\.)'| sort | uniq
