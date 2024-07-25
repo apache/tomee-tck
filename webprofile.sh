1ca40297003c49faa9e80381a39811ccd0712682
Paulo Cristovão de Araújo Silva Filho <pcristof@gmail.com>
2024-07-25 15:33:50 -0300

    Updating webprofile.sh to reflect jakarta-10 web profile requirements
    Requirements obtained from https://jakarta.ee/specifications/webprofile/10/jakarta-webprofile-spec-10.0#a43 and by comparing current web profile certification of tomee (9.1) with glassfish-7 (10) one.

--- webprofile.sh
+++ webprofile.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
 ./runtests "$@" \
+    com.sun.ts.tests.appclient \
+    com.sun.ts.tests.assembly \
+    com.sun.ts.tests.connector \
+    com.sun.ts.tests.ejb \
+    com.sun.ts.tests.ejb30.assembly \
+    com.sun.ts.tests.ejb30.bb \
     com.sun.ts.tests.ejb30.lite.appexception \
+    com.sun.ts.tests.ejb30.lite.async \
     com.sun.ts.tests.ejb30.lite.basic \
     com.sun.ts.tests.ejb30.lite.ejbcontext \
     com.sun.ts.tests.ejb30.lite.enventry \
     com.sun.ts.tests.ejb30.lite.interceptor \
     com.sun.ts.tests.ejb30.lite.lookup \
+    com.sun.ts.tests.ejb30.lite.naming \
     com.sun.ts.tests.ejb30.lite.nointerface \
     com.sun.ts.tests.ejb30.lite.packaging \
     com.sun.ts.tests.ejb30.lite.singleton \
     com.sun.ts.tests.ejb30.lite.stateful \
     com.sun.ts.tests.ejb30.lite.tx \
     com.sun.ts.tests.ejb30.lite.view \
-    com.sun.ts.tests.ejb30.persistence \
+    com.sun.ts.tests.ejb30.lite.xmloverride \
+    com.sun.ts.tests.ejb30.misc \
+    com.sun.ts.tests.ejb30.sec \
+    com.sun.ts.tests.ejb30.timer \
+    com.sun.ts.tests.ejb30.webservice \
+    com.sun.ts.tests.ejb30.zombie \
+    com.sun.ts.tests.ejb32 \
+    com.sun.ts.tests.el \
+    com.sun.ts.tests.integration \
+    com.sun.ts.tests.jacc \
+    com.sun.ts.tests.javaee \
+    com.sun.ts.tests.javamail \
+    com.sun.ts.tests.jaxrs \
+    com.sun.ts.tests.jaxrs \
     com.sun.ts.tests.jdbc.ee.batchUpdate \
     com.sun.ts.tests.jdbc.ee.callStmt \
     com.sun.ts.tests.jdbc.ee.connection \
@@ -25,30 +46,14 @@
     com.sun.ts.tests.jdbc.ee.resultSet \
     com.sun.ts.tests.jdbc.ee.rsMeta \
     com.sun.ts.tests.jdbc.ee.stmt \
-    com.sun.ts.tests.jsf.api.javax_faces.application \
-    com.sun.ts.tests.jsf.api.javax_faces.component \
-    com.sun.ts.tests.jsf.api.javax_faces.context \
-    com.sun.ts.tests.jsf.api.javax_faces.convert \
-    com.sun.ts.tests.jsf.api.javax_faces.el \
-    com.sun.ts.tests.jsf.api.javax_faces.event \
-    com.sun.ts.tests.jsf.api.javax_faces.facesexception \
-    com.sun.ts.tests.jsf.api.javax_faces.factoryfinder \
-    com.sun.ts.tests.jsf.api.javax_faces.factoryfinderrelease \
-    com.sun.ts.tests.jsf.api.javax_faces.lifecycle \
-    com.sun.ts.tests.jsf.api.javax_faces.model \
-    com.sun.ts.tests.jsf.api.javax_faces.render \
-    com.sun.ts.tests.jsf.api.javax_faces.validator \
-    com.sun.ts.tests.jsf.api.javax_faces.view \
-    com.sun.ts.tests.jsf.spec \
+    com.sun.ts.tests.jms \
     com.sun.ts.tests.jsp \
     com.sun.ts.tests.jstl \
     com.sun.ts.tests.jta \
     com.sun.ts.tests.samples \
     com.sun.ts.tests.servlet \
     com.sun.ts.tests.signaturetest \
-    com.sun.ts.tests.el \
-    com.sun.ts.tests.connector \
-    com.sun.ts.tests.jacc \
-    com.sun.ts.tests.javamail \
-    com.sun.ts.tests.samples.javamail
-
+    com.sun.ts.tests.webservices12 \
+    com.sun.ts.tests.webservices13 \
+    com.sun.ts.tests.websocket \
+    com.sun.ts.tests.xa
